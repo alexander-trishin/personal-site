3c2dc35f3a4a288ffb62ef6226f6a66ed9d8c1c7
Alexander Trishin <Alexander_Trishin@mail.ru>
2022-04-21 14:41:52 +0300

    Add pino logger with logflare integration

---
+++ src/common/logger.ts
@@ -0,0 +1,21 @@
+import pino from 'pino';
+import { logflarePinoVercel } from 'pino-logflare';
+
+const { send, stream } = logflarePinoVercel({
+    apiKey: process.env.LOGFLARE_API_KEY!,
+    sourceToken: process.env.LOGFLARE_SOURCE_TOKEN!
+});
+
+const logger = pino(
+    {
+        browser: { transmit: { send } },
+        level: 'error',
+        base: {
+            env: process.env.VERCEL_ENV,
+            revision: process.env.VERCEL_GITHUB_COMMIT_SHA
+        }
+    },
+    stream
+);
+
+export default logger;
