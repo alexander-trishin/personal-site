3a294ad142230013da0f00f13317476b09050ad7
Alexander Trishin <Alexander_Trishin@mail.ru>
2022-06-26 16:14:02 +0300

    Add send-email api route

---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+import { createController, HttpAction } from 'server/api';
+import { sendEmail } from 'server/email';
+import { HttpStatus } from 'shared/constants';
+import { trim } from 'shared/utils/string';
+
+const controller = createController(server => {
+    const postSchema = z.object({
+        body: z.object({
+            name: z.optional(z.preprocess(trim, z.string().max(64))),
+            email: z.preprocess(trim, z.string().min(5).max(64).email()),
+            subject: z.preprocess(trim, z.string().min(1).max(128)),
+            message: z.preprocess(trim, z.string().min(8).max(2048))
+        })
+    });
+
+    const postHandler: HttpAction = async (request, response) => {
+        const {
+            body: { name, email, subject, message }
+        } = await postSchema.parseAsync(request);
+
+        await sendEmail({
+            to: 'Alexander_Trishin@mail.ru',
+            subject,
+            html: `<div><p>From: ${
+                name ? `<${name}> ` : ''
+            }${email}</p><br /><p>${message}</p></div>`
+        });
+
+        response.status(HttpStatus.Code204NoContent).end();
+    };
+
+    server.post(postHandler);
+});
+
+export default controller;
